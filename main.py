2d1999296283d6a33b6792c73cacea19c73babd3
Alan Zarza <tvandmovies300@gmail.com>
2024-05-13 14:55:32 -0700

    print out report in specified format

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 def main():
-    text = getBookContent("books/frankenstein.txt")
-    print(text)
-    print(count_words(text))
+    path = "books/frankenstein.txt"
+    text = getBookContent(path)
+    word_count = count_words(text)
     counts = count_letters(text)
-    print(counts)
+
+    print(f"---Begin report of {path} ---")
+    print(f"{word_count} words found in document")
+
+    list = count_letters(text)
+
+    reverse(list)
+
+    for i in list:
+        print(f"The '{i["name"]}' character was found {i["num"]} times")
+        
+    print("--- End Report ---")
+
 
 def getBookContent(path):
     with open(path) as f:
@@ -15,9 +27,19 @@ def count_words(text):
 
 def count_letters(text):
     text = text.lower()
-    counts = {}
-    for i in range(ord('a'),ord('z')):
-        counts[chr(i)] = text.count(chr(i))
-    return counts
+    count_list = []
+    for i in range(ord('a'),ord('z')+1):
+        counts = {
+            "name":chr(i),
+            "num":text.count(chr(i))
+        }
+        count_list.append(counts)
+    return count_list
+
+def sort_on(dic):
+    return dic["num"]
+
+def reverse(list):
+    list.sort(reverse=True,key=sort_on)
     
 main()
